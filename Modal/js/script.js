c42f1957eeb13fd178d396777af8b7a9a1796735
Lois Santos <loisdilone@gmail.com>
2017-02-14 17:47:43 -0600

    Ejercicio 3
    Modal Pop up

---
+++ Modal/js/script.js
@@ -0,0 +1,71 @@
+// S AND M
+var modal = document.getElementById('pop');
+
+// Get the button that opens the modal
+var btn = document.getElementById("but");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("cerrar")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Dave
+var modal = document.getElementById('pop');
+
+var btn = document.getElementById("but");
+
+var span = document.getElementsByClassName("cerrar")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+// Tati
+var modal = document.getElementById('pop');
+
+
+var btn = document.getElementById("but");
+
+
+var span = document.getElementsByClassName("cerrar")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
