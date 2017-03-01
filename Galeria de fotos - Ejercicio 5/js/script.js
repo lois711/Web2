1a9371a256d5cadfb1648ff071f578569ea5083c
Lois Santos <loisdilone@gmail.com>
2017-02-28 18:31:33 -0600

    Ejercicio 5
    Ejercicio 5
    
    je

---
+++ Galeria de fotos - Ejercicio 5/js/script.js
@@ -0,0 +1,176 @@
+//PICTURE1
+
+var modal = document.getElementById('pop');
+
+var btn = document.getElementById("but");
+
+var span = document.getElementsByClassName("cerrar")[0];
+ 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// PICTURE2
+
+var modal2 = document.getElementById('pop2');
+
+var btn = document.getElementById("but2");
+
+var span = document.getElementsByClassName("cerrar2")[0];
+ 
+btn.onclick = function() {
+    modal2.style.display = "block";
+}
+
+span.onclick = function() {
+    modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
+
+// PICTURE3
+
+var modal3 = document.getElementById('pop3');
+
+var btn = document.getElementById("but3");
+
+var span = document.getElementsByClassName("cerrar3")[0];
+ 
+btn.onclick = function() {
+    modal3.style.display = "block";
+}
+
+span.onclick = function() {
+    modal3.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+}
+
+// PICTURE4
+
+var modal4 = document.getElementById('pop4');
+
+var btn = document.getElementById("but4");
+
+var span = document.getElementsByClassName("cerrar4")[0];
+ 
+btn.onclick = function() {
+    modal4.style.display = "block";
+}
+
+span.onclick = function() {
+    modal4.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal4) {
+        modal4.style.display = "none";
+    }
+}
+
+// PICTURE5
+
+var modal5 = document.getElementById('pop5');
+
+var btn = document.getElementById("but5");
+
+var span = document.getElementsByClassName("cerrar5")[0];
+ 
+btn.onclick = function() {
+    modal5.style.display = "block";
+}
+
+span.onclick = function() {
+    modal5.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal5) {
+        modal5.style.display = "none";
+    }
+}
+
+// PICTURE6
+
+var modal6 = document.getElementById('pop6');
+
+var btn = document.getElementById("but6");
+
+var span = document.getElementsByClassName("cerrar6")[0];
+ 
+btn.onclick = function() {
+    modal6.style.display = "block";
+}
+
+span.onclick = function() {
+    modal6.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal6) {
+        modal6.style.display = "none";
+    }
+}
+
+// PICTURE7
+
+var modal7 = document.getElementById('pop7');
+
+var btn = document.getElementById("but7");
+
+var span = document.getElementsByClassName("cerrar7")[0];
+ 
+btn.onclick = function() {
+    modal7.style.display = "block";
+}
+
+span.onclick = function() {
+    modal7.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal7) {
+        modal7.style.display = "none";
+    }
+}
+
+// PICTURE8
+
+var modal8 = document.getElementById('pop8');
+
+var btn = document.getElementById("but8");
+
+var span = document.getElementsByClassName("cerrar8")[0];
+ 
+btn.onclick = function() {
+    modal8.style.display = "block";
+}
+
+span.onclick = function() {
+    modal8.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal8) {
+        modal8.style.display = "none";
+    }
+}
+
